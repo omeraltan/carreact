e901a88d1d655be19653041db12482b384f69b71
Ömer ALTAN <omeraltan66@gmail.com>
2022-06-25 18:22:02 +0300

    Update Columns

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React, {useState, useRef} from 'react';
-
+import React, { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import './App.css';
 
 function App() {
-  const [user, setUser] = useState();
-  let userName = useRef();
+  const [keyword, setKeyword] = useState('');
+  const [data, setData] = useState([]);
 
-  const showUserName = () => {
-    if (userName != null && userName !== "") {
-      setUser(userName.current.value);
-    } else {
-      setUser(userName.current.value);
-    }
-    
-  }
+  const columns = [
+    {field: 'full_name', sortable: true, filter:
+    true},
+    {field: 'html_url', sortable: true, filter: true},
+    {field: 'owner.login', sortable: true, filter: true},
+    {
+      field: 'full_name',
+      cellRenderer: params =>
+      <button
+      onClick={() => alert(params.value)}>
+      Press me
+      </button>
+      }
+  ]
 
+  const fetchData = () => {
+    fetch(`https://api.github.com/search/repositories?q=${keyword}`)
+      .then(response => response.json())
+      .then(data => setData(data.items))
+      .catch(err => console.error(err));
+  };
   return (
-    <div>
-      <p>User Name: {user}</p>
-      <input type="text" ref={userName}></input>
-      <button onClick={showUserName}> Show User Name</button>
+    <div className="App">
+      <input value={keyword} onChange={e => setKeyword(e.target.value)} />
+      <button onClick={fetchData}>Fetch</button>
+      <div className="ag-theme-material" style={{ height: 500, width: '90%' }}>
+        <AgGridReact rowData={data} columnDefs={columns} pagination={true} paginationPageSize={8}/>
+      </div>
     </div>
   );
 }
